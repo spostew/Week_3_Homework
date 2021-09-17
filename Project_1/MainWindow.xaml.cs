6822b7f2076d8b7d4ac9311370b16d5a1df0cca5
spostew <spencerunitt0@gmail.com>
2021-09-17 14:57:44 -0700

    Add project files.

---
+++ Project_1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Project_1: This program will have the user enter in a monthly payment and total loan price. It will return the cost that will cost to 
+ *            interest and the amount that will go to the principal.
+ * Name: Spencer Unitt
+ * Module: Homework_3, Project 1
+ * Problem Statement: Create a program that will display a monthly morgtage payment after recieving the payment and total loan amount
+ * 
+ * Algorithm
+ *  1. Create a const varible that holds the interest amount in decimal format. Store the interest rate inside
+ *  2. Create two variables that will parse the data from the textBoxes in the GUI. One should hold the payment value, the other
+ *     should hold the total loan amount
+ *  3. Use the following equation to determine the interest amount
+ *     monthly_interest = loan * (1 + interest_rate * 1). subtract this total by the loan amount
+ *  4. Subtract the payment by the monthly interest and store in a variable for the principal amount
+ *  5. Display the amount that will go to the interest rate and the amount that goes to the pricipal
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void generate_amount_left_Click(object sender, RoutedEventArgs e)
+        {
+            // Creates a variable that can't be changed set to the interest rate converted to deciaml format
+            const double interest_rate = 6.39 / 100;
+
+            // Gets the values in the textBoxes
+            double payment = Double.Parse(monthly_Payment.Text);
+            double debt = Double.Parse(amount_Owed.Text);
+            
+            // Calculates the monthly interest
+            double monthly_interest = debt * (1 + interest_rate * 1);
+            monthly_interest -= debt;
+
+            // Calculates the amount that goes to principal
+            double monthly_principal = payment - monthly_interest;
+
+            // Displays the results
+            amount_To_Interest.Text = "$" + monthly_interest.ToString();
+            amount_To_Principal.Text = "$" + monthly_principal.ToString();
+
+        }
+
+        
+    }
+}
---
+++ Project_2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Project_2: This program will calculate how much change is returned from a vending machine after a dollar has been entered.
+ * Name: Spencer Unitt
+ * Module: Homework 3, Project 2
+ * Problem Statement: Return the remainder of change in each coin after an item has been bought from a vending machine
+ * 
+ * Algorithm: 
+ *  1. Create a text box that prompts the user for the cost of the item they wish to pay for.
+ *  2. When a button below the textbox is clicked, do the following
+ *      2a. Create a variable that takes in the number from the textBox
+ *      2b. Create variables for each coin with the appropriate value. And variables named (the coin)_amount. These will hold the remainder.
+ *      2c. Subtract 100 (a dollar) from the cost of the item
+ *      2d. Use the following two equations for each coin
+ *          coin_amount = change / coin_value
+ *          change = change - (coin_value * coin_amount)
+ *      2e. In a seperate textBlock return the amount each coin will be given based on the value the user enters.
+ *  3. Have a spot for the text to display when the user clicks the approprate button. 
+ *  4. Create an exit button that when clicked will close the program for the user. *   
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void recieve_Change_Click(object sender, RoutedEventArgs e)
+        {
+            // Variables used throughout 
+            int change = int.Parse(change_insert.Text);
+            // Holds the value of each coin
+            int quarter = 25, nickle = 5, dime = 10;
+            // Holds the remainder and total intial cost
+            int quarter_amount, dime_amount, nickle_amount, penny_amount, total_cost = change;
+
+            // Calculates the starting balance
+            change = 100 - change;
+            // Determines the remainder
+            quarter_amount = change / 25;
+            // Updates the current balance and repeats until it reaches 0
+            change -= quarter * quarter_amount;
+            dime_amount = change / 10;
+            change -= dime * dime_amount;
+            nickle_amount = change / 5;
+            change -= nickle * nickle_amount;
+            penny_amount = change;
+
+            // Returns the values of everything and displays to user in a TextBlock
+            return_Change.Text = "The item cost " + total_cost + " cents.\n"
+                            + "After inserting a dollar you will recieve the following change.\n\n"
+                            + "Quarters: " + quarter_amount.ToString() + "\n" 
+                            + "Dimes: " + dime_amount.ToString() + "\n" 
+                            + "Nickles: " + nickle_amount.ToString() + "\n"
+                            + "Pennies: " + penny_amount.ToString();
+                   
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Closes the program if user clicks this button
+            this.Close();
+        }
+    }
+}
